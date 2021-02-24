8b2d5f09712da6537b2d07abd3831e834689e45e
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-02-24 22:05:47 +0100

    Added try & except

--- fahrtocel.py
+++ fahrtocel.py
@@ -1,6 +1,10 @@
 print('Convert Fahrenheit to Celsius')
 temp = input("What is the temperature in Fahrenheit? ")
 cel = 0
-fahr = float(temp)
+try:
+    fahr = float(temp)
+except:
+    print("That's not a number")
+    quit()
 cel = (fahr - 32.0) * 5.0 / 9.0
 print("The temperature in Celsius is " + str(cel))
