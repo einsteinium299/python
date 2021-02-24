7ac69fa80031b04bc31e85b776a543e4b1c70dfb
Einsteinium <39045294+einsteinium299@users.noreply.github.com>
2021-02-24 21:50:26 +0100

    Fahrenheit to Celsius

---
+++ fahrtocel.py
@@ -0,0 +1,6 @@
+print('Convert Fahrenheit to Celsius')
+temp = input("What is the temperature in Fahrenheit? ")
+cel = 0
+fahr = float(temp)
+cel = (fahr - 32.0) * 5.0 / 9.0
+print("The temperature in Celsius is " + str(cel))
